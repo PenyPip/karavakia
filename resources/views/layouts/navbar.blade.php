651996c1104ba3ff8f4a624c0e7e6ad70e9659f3
fotistog <fotistog@gmail.com>
2022-07-12 16:22:35 +0300

    passengerdetails +footer + nav

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,112 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Landing Page - Start Bootstrap Theme</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
+
+    <link rel="stylesheet" href="js/jquery-ui.css">
+
+    <style>
+        .btn-circle.btn-xl {
+            width: 70px;
+            height: 70px;
+            padding: 13px 18px;
+            border-radius: 60px;
+            font-size: 15px;
+            text-align: center;
+            margin-right: 10px;
+        }
+
+        .container-counter {
+            margin-top: 100px;
+            margin-bottom: 100px;
+            padding-left: 25px;
+            padding-right: 25px;
+        }
+
+        .counter-box {
+            display: block;
+            background: #0b3f82;
+            padding: 40px 20px 37px;
+            text-align: center;
+            color: #fff;
+        }
+
+        .counter-box p {
+            margin: 5px 0 0;
+            padding: 0;
+            color: #fff;
+            font-size: 18px;
+            font-weight: 500
+        }
+
+        .counter-box i {
+            font-size: 60px;
+            margin: 0 0 15px;
+            color: #fff;
+
+        }
+
+        .counter {
+            display: block;
+            font-size: 32px;
+            font-weight: 700;
+            color: #fff;
+            line-height: 28px
+        }
+
+        .counter-box.colored {
+            background: #0b3f82;
+        }
+
+        .counter-box.colored p,
+        .counter-box.colored i,
+        .counter-box.colored .counter {
+            color: #fff
+        }
+    </style>
+</head>
+<!-- Navigation-->
+
+<nav class="navbar navbar-light bg-custom static-top">
+        <div class="container">
+
+        <a class="navbar-brand" href="#">
+                <img src="images/pegasusLogo.png" alt="Girl in a jacket" width="190" height="60">
+            </a>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">HOME</a>
+            </nav>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">CRUISES</a>
+            </nav>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">DESTINATIONS</a>
+            </nav>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">INFORMATION</a>
+            </nav>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">SHIPS</a>
+            </nav>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">NEWS</a>
+            </nav>
+            <nav class="navbar navbar-light">
+                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">CONTACT</a>
+            </nav>
+            <a class="btn btn-primary" href="#signup">Sign Up</a>
+        </div>
+    </nav>
---
+++ resources/views/passengerDetails.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layouts.navbar')
+
+<body>
+    <!-- Masthead-->
+    <header class="passengerDetails">
+
+
+        <div class="container ml-4 py-3">
+            <div class="row">
+                <div class="col-lg-6 ml-4 mb-4">
+                    
+                        <div class="card card-body  ">
+                            <h3 class="text-center mb-4">Επιβάτης 1 (Leader)</h3>
+
+                            <fieldset>
+                                <div class="form-group has-error mb-4">
+                                    <input class="form-control input-lg" placeholder="E-mail Address" name="email" type="text">
+                                </div>
+                                <div class="form-group has-success mb-4">
+                                    <input class="form-control input-lg" placeholder="Password" name="password" value="" type="password">
+                                </div>
+                                <div class="form-group has-success mb-4">
+                                    <input class="form-control input-lg" placeholder="Confirm Password" name="password" value="" type="password">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <select class="form-control input-lg">
+                                        <option selecterd="">Sequrity Question</option>
+                                    </select>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control input-lg" placeholder="Sequrity Answer" name="answer" value="" type="text">
+                                </div>
+                                
+                            </fieldset>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 mx-auto">
+                        <div class="card card-body">
+                            <h3 class="text-center mb-4">Επιβάτης 1 (Leader)</h3>
+
+                            <fieldset>
+                                <div class="form-group has-error mb-4">
+                                    <input class="form-control input-lg" placeholder="E-mail Address" name="email" type="text">
+                                </div>
+                                <div class="form-group has-success mb-4">
+                                    <input class="form-control input-lg" placeholder="Password" name="password" value="" type="password">
+                                </div>
+                                <div class="form-group has-success mb-4">
+                                    <input class="form-control input-lg" placeholder="Confirm Password" name="password" value="" type="password">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <select class="form-control input-lg">
+                                        <option selecterd="">Sequrity Question</option>
+                                    </select>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control input-lg" placeholder="Sequrity Answer" name="answer" value="" type="text">
+                                </div>
+                                
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                <div class="col-lg-6 ml-4">
+                    
+                        <div class="card card-body  ">
+                            <h3 class="text-center mb-4">Επιβάτης 2 </h3>
+
+                            <fieldset>
+                                <div class="form-group has-error mb-4">
+                                    <input class="form-control input-lg" placeholder="E-mail Address" name="email" type="text">
+                                </div>
+                                <div class="form-group has-success mb-4">
+                                    <input class="form-control input-lg" placeholder="Password" name="password" value="" type="password">
+                                </div>
+                                <div class="form-group has-success mb-4">
+                                    <input class="form-control input-lg" placeholder="Confirm Password" name="password" value="" type="password">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <select class="form-control input-lg">
+                                        <option selecterd="">Sequrity Question</option>
+                                    </select>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control input-lg" placeholder="Sequrity Answer" name="answer" value="" type="text">
+                                </div>
+                               
+                            </fieldset>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
+
+
+    </header>
+</body>
+
+@include('layouts.footer')
+
+
+</html>
