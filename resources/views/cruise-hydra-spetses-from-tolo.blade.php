836a5f517f2042c20850bccd88f5afafba6135b1
manospapyr <m.papyrakis@gmail.com>
2022-07-14 12:48:03 +0300

    Updated structure & Added payment

--- resources/views/cruise-hydra-spetses-from-tolo.blade.php
+++ resources/views/cruise-hydra-spetses-from-tolo.blade.php
@@ -1,124 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Landing Page - Start Bootstrap Theme</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
-
-    <link rel="stylesheet" href="js/jquery-ui.css">
-
-    <style>
-        .btn-circle.btn-xl {
-            width: 70px;
-            height: 70px;
-            padding: 13px 18px;
-            border-radius: 60px;
-            font-size: 15px;
-            text-align: center;
-            margin-right: 10px;
-        }
-
-        .container-counter {
-            margin-top: 100px;
-            margin-bottom: 100px;
-            padding-left: 25px;
-            padding-right: 25px;
-        }
-
-        .counter-box {
-            display: block;
-            background: #0b3f82;
-            padding: 40px 20px 37px;
-            text-align: center;
-            color: #fff;
-        }
-
-        .counter-box p {
-            margin: 5px 0 0;
-            padding: 0;
-            color: #fff;
-            font-size: 18px;
-            font-weight: 500
-        }
-
-        .counter-box i {
-            font-size: 60px;
-            margin: 0 0 15px;
-            color: #fff;
-
-        }
-
-        .counter {
-            display: block;
-            font-size: 32px;
-            font-weight: 700;
-            color: #fff;
-            line-height: 28px
-        }
-
-        .counter-box.colored {
-            background: #0b3f82;
-        }
-
-        .counter-box.colored p,
-        .counter-box.colored i,
-        .counter-box.colored .counter {
-            color: #fff
-        }
-    </style>
-</head>
+@include('layouts.navbar')
 
 <body>
-    <!-- Navigation-->
-
-    <nav class="navbar navbar-light bg-custom static-top">
-        <div class="container">
-
-            <a class="navbar-brand" href="#">
-                <img src="images/pegasusLogo.png" alt="Girl in a jacket" width="190" height="60">
-            </a>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="/homesearch" style="color: #fff; font-size: 15px;">HOME</a>
-            </nav>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="/passengerDetails" style="color: #fff; font-size: 15px;">CRUISES</a>
-            </nav>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">DESTINATIONS</a>
-            </nav>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">INFORMATION</a>
-            </nav>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">SHIPS</a>
-            </nav>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">NEWS</a>
-            </nav>
-            <nav class="navbar navbar-light">
-                <a class="navbar-brand" href="#" style="color: #fff; font-size: 15px;">CONTACT</a>
-            </nav>
-            <a class="btn btn-primary" href="#signup">Sign Up</a>
-        </div>
-    </nav>
 
 
     <!--Product Content-->
-    <img src="/files/images/upperdeck1920.jpg" style="background-repeat:no-repeat; background-size:cover; width:100%; height:140px;">
+    <img src="/images/bg-cruises-top.jpg" style="background-repeat:no-repeat; background-size:cover; width:100%; height:140px;">
     <div class="container">
         <div class="tour" style="display:grid; grid-template-columns: 2fr 1fr;">
             <div class="tour-content" style="margin-top:50px; background-color:#F2F2F2; border-radius:8px; padding:15px;">
@@ -137,7 +26,7 @@
                     <p>(Social media icons)</p>
                 </div>
                 <div>
-                    <img src="/files/images/udra-820x500.jpg" style="width: 100%; height: auto;">
+                    <img src="/images/product-hydra.jpg" style="width: 100%; height: auto;">
                 </div>
                 <div>
                     <h3 style="margin-top:20px;">Description</h3>
@@ -158,37 +47,7 @@
 
     </div>
     <!-- Footer-->
-    <footer class="footer bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
-                    <ul class="list-inline mb-2">
-                        <li class="list-inline-item"><a href="#!">About</a></li>
-                        <li class="list-inline-item">⋅</li>
-                        <li class="list-inline-item"><a href="/contact-us">Contact</a></li>
-                        <li class="list-inline-item">⋅</li>
-                        <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
-                        <li class="list-inline-item">⋅</li>
-                        <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
-                    </ul>
-                    <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2022. All Rights Reserved.</p>
-                </div>
-                <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
-                    <ul class="list-inline mb-0">
-                        <li class="list-inline-item me-4">
-                            <a href="#!"><i class="bi-facebook fs-3"></i></a>
-                        </li>
-                        <li class="list-inline-item me-4">
-                            <a href="#!"><i class="bi-twitter fs-3"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#!"><i class="bi-instagram fs-3"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('layouts.footer')
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
