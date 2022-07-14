a40df465a7caaa1f01f3fa52f9daab0dbbf8458f
manospapyr <m.papyrakis@gmail.com>
2022-07-14 10:13:31 +0300

    fixed footer in a cruise page

--- resources/views/cruise-hydra-spetses-from-tolo.blade.php
+++ resources/views/cruise-hydra-spetses-from-tolo.blade.php
@@ -116,6 +116,47 @@
         </div>
     </nav>
 
+
+    <!--Product Content-->
+    <img src="/files/images/upperdeck1920.jpg" style="background-repeat:no-repeat; background-size:cover; width:100%; height:140px;">
+    <div class="container">
+        <div class="tour" style="display:grid; grid-template-columns: 2fr 1fr;">
+            <div class="tour-content" style="margin-top:50px; background-color:#F2F2F2; border-radius:8px; padding:15px;">
+                <h1 style="font-weight:bold; font-size:24px;">
+                    HYDRA & SPETSES FROM TOLO
+
+                </h1>
+                <p style="text-align:right;">
+                    Code: YDRASPETSES-GR
+                </p>
+                </hr>
+                <div style="display:grid; grid-template-columns: 1fr 1fr 1fr">
+                    <p>One-day Cruise
+                    </p>
+                    <p><a href="#">ARGOSARONIKOS</a>, <a href="#">HYDRA</a>, <a href="#">SPETSES</a></p>
+                    <p>(Social media icons)</p>
+                </div>
+                <div>
+                    <img src="/files/images/udra-820x500.jpg" style="width: 100%; height: auto;">
+                </div>
+                <div>
+                    <h3 style="margin-top:20px;">Description</h3>
+                    </hr>
+                    <p>One day cruise with Pegasus Cruises. Departure from Tolo for the picturesque island of Hydra with its unique singularity: the absence of cars on the island and the beautiful island of Spetses with its many trees, bays and beaches.
+
+                        Escape from the everyday routine and enjoy the islands, the sun and the sea with Pegasus Cruises. You have the opportunity to spend 3 hours in Hydra and 2 hours in Spetses.
+
+
+                        You will have plenty of time to have a swim, wonder around the stone streets of the islands and enjoy tasty greek food at the various tavernas. While on board you can enjoy the amenities that our ships provide and relax in both our open and closed lounges.</p>
+                </div>
+            </div>
+            <div class="tour-booking-panel" margin-top: -25px>
+                <p style="padding:10px; background-color: #FDB200; height: 50px; width: 280px; text-align: center; margin-top: -25px; color: #fff; font-size: 20px;">Price: <font style="font-weight:bold;">28€</font>
+                </p>
+            </div>
+        </div>
+
+    </div>
     <!-- Footer-->
     <footer class="footer bg-light">
         <div class="container">
@@ -158,46 +199,6 @@
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 
-    <!--Product Content-->
-    <img src="/files/images/upperdeck1920.jpg" style="background-repeat:no-repeat; background-size:cover; width:100%; height:140px;">
-    <div class="container">
-        <div class="tour" style="display:grid; grid-template-columns: 2fr 1fr;">
-            <div class="tour-content" style="margin-top:50px; background-color:#F2F2F2; border-radius:8px; padding:15px;">
-                <h1 style="font-weight:bold; font-size:24px;">
-                    HYDRA & SPETSES FROM TOLO
-
-                </h1>
-                <p style="text-align:right;">
-                    Code: YDRASPETSES-GR
-                </p>
-                </hr>
-                <div style="display:grid; grid-template-columns: 1fr 1fr 1fr">
-                    <p>One-day Cruise
-                    </p>
-                    <p><a href="#">ARGOSARONIKOS</a>, <a href="#">HYDRA</a>, <a href="#">SPETSES</a></p>
-                    <p>(Social media icons)</p>
-                </div>
-                <div>
-                    <img src="/files/images/udra-820x500.jpg" style="width: 100%; height: auto;">
-                </div>
-                <div>
-                    <h3 style="margin-top:20px;">Description</h3>
-                    </hr>
-                    <p>One day cruise with Pegasus Cruises. Departure from Tolo for the picturesque island of Hydra with its unique singularity: the absence of cars on the island and the beautiful island of Spetses with its many trees, bays and beaches.
-
-                        Escape from the everyday routine and enjoy the islands, the sun and the sea with Pegasus Cruises. You have the opportunity to spend 3 hours in Hydra and 2 hours in Spetses.
-
-
-                        You will have plenty of time to have a swim, wonder around the stone streets of the islands and enjoy tasty greek food at the various tavernas. While on board you can enjoy the amenities that our ships provide and relax in both our open and closed lounges.</p>
-                </div>
-            </div>
-            <div class="tour-booking-panel" margin-top: -25px>
-                <p style="padding:10px; background-color: #FDB200; height: 50px; width: 280px; text-align: center; margin-top: -25px; color: #fff; font-size: 20px;">Price: <font style="font-weight:bold;">28€</font>
-                </p>
-            </div>
-        </div>
-
-    </div>
 </body>
 
 </html>
