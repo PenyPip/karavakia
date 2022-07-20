71244211ff334e6a88e13aaca1811481883edba9
fotistog <fotistog@gmail.com>
2022-07-20 16:23:41 +0300

    missing css

--- resources/views/adminb2b.blade.php
+++ resources/views/adminb2b.blade.php
@@ -12,7 +12,7 @@
     <title>SB Admin 2 - Tables</title>
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -22,7 +22,7 @@
 
     <!-- Custom styles for this page -->
     <link href="css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="css/jquery.dataTables.min.css" rel="stylesheet">
 
 
 </head>
